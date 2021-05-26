ff610f32a9bc5ea62bbea0344ee692e4a8251eb9
Youssef1313 <youssefvictor00@gmail.com>
2021-05-26 20:13:24 +0200

    Fix

--- src/RedirectionVerifier/OpenPublishingRedirections.cs
+++ src/RedirectionVerifier/OpenPublishingRedirections.cs
@@ -3,7 +3,9 @@ using System.Text.Json.Serialization;
 
 namespace RedirectionVerifier
 {
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes → JSON
     internal sealed class OpenPublishingRedirections
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
     {
         [JsonPropertyName("redirections")]
         public ImmutableArray<Redirection> Redirections { get; set; }
--- src/RedirectionVerifier/Redirection.cs
+++ src/RedirectionVerifier/Redirection.cs
@@ -2,14 +2,14 @@
 
 namespace RedirectionVerifier
 {
-    public sealed class Redirection
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes → JSON
+    internal sealed class Redirection
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
     {
         [JsonPropertyName("source_path")]
         public string SourcePath { get; set; } = null!;
 
         [JsonPropertyName("redirect_url")]
-#pragma warning disable CA1056 // URI-like properties should not be strings → https://github.com/dotnet/roslyn-analyzers/issues/5133
         public string RedirectUrl { get; set; } = null!;
-#pragma warning restore CA1056 // URI-like properties should not be strings
     }
 }
