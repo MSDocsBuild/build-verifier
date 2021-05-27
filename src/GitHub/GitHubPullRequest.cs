ffa9b7e0df163f37a8336863c28f89745923b0ff
Youssef Victor <youssefvictor00@gmail.com>
2021-05-27 07:24:59 +0200

    Add redirection validator to action runner (#5)
    * Add redirection validator to action runner
    
    * Update Dockerfile
    
    * Fix comment
    
    * Use token

--- src/GitHub/GitHubPullRequest.cs
+++ src/GitHub/GitHubPullRequest.cs
@@ -7,17 +7,22 @@ using Octokit;
 
 namespace GitHub
 {
-    public static class PullRequest
+    public static class GitHubPullRequest
     {
         // Documented in https://docs.github.com/en/actions/reference/environment-variables#default-environment-variables.
         private const string GITHUB_REPO_ENV_VARIABLE = "GITHUB_REPOSITORY";
+
+        // env:
+        //   GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        private const string GITHUB_TOKEN_ENV_VARIABLE = "GITHUB_TOKEN";
 
         /// <summary>
         /// Given a pull request number, retrieve the files.
         /// </summary>
         /// <param name="pullRequestNumber">The number of the target pull request.</param>
         /// <exception cref="InvalidOperationException">
-        /// The exception is thrown if 'GITHUB_REPOSITORY' environment variable isn't found, or if its value is invalid (doesn't contain exactly one slash).
+        /// <para>The exception is thrown if 'GITHUB_REPOSITORY' environment variable isn't found, or if its value is invalid (doesn't contain exactly one slash).</para>
+        /// <para>The exception is thrown if 'GITHUB_TOKEN' environment variable isn't found.</para>
         /// </exception>
         public static async Task<IEnumerable<PullRequestFile>> GetPullRequestFilesAsync(int pullRequestNumber)
         {
@@ -25,6 +30,12 @@ namespace GitHub
             if (repository is null)
             {
                 throw new InvalidOperationException($"Environment variable '{GITHUB_REPO_ENV_VARIABLE}' was not found.");
+            }
+
+            string? token = Environment.GetEnvironmentVariable(GITHUB_TOKEN_ENV_VARIABLE);
+            if (token is null)
+            {
+                throw new InvalidOperationException($"Environment variable '{GITHUB_TOKEN_ENV_VARIABLE}' was not found.");
             }
 
             if (repository.Count(c => c == '/') != 1)
@@ -37,8 +48,10 @@ namespace GitHub
             string user = userAndRepo[0];
             string repo = userAndRepo[1];
 
-            var client = new GitHubClient(new ProductHeaderValue("MSDocsBuildVerifier"));
-            // TODO: access token for potentially larger rate limit??
+            var client = new GitHubClient(new ProductHeaderValue("MSDocsBuildVerifier"))
+            {
+                Credentials = new Credentials(token)
+            };
 
             IReadOnlyList<PullRequestFile> files = await client.PullRequest.Files(user, repo, pullRequestNumber).ConfigureAwait(false);
             return files;
